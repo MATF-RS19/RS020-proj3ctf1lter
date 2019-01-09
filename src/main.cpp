dca48ec1a2f6d488ee865ea3cbfbb205633dfa8f
deMand_A <mandinikola@gmail.com>
2019-01-09 20:05:42 +0100

    refactored, added loss computing (prolly wrong)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #endif  // USE_OPENCV
 #include <algorithm>
+#include <numeric>
 #include <iosfwd>
 #include <memory>
 #include <string>
@@ -12,14 +13,11 @@
 #include <vector>
 #include <ostream>
 
-#define TYPE (0)
-#define OUTPUT_STARTING_IMAGE (1)
-
 #ifdef USE_OPENCV
 using namespace caffe;  // NOLINT(build/namespaces)
 using std::string;
 
-std::vector<float> entering_image;
+vector<float> entering_image;
 
 class Compressor {
 public:
@@ -27,17 +25,16 @@ public:
 		   const string& trained_file,
 		   const string& mean_file);
 
-	std::vector<float> compress(const cv::Mat& img);
-
-	void decompress();
+	vector<float> compress(const cv::Mat& img);
 
 private:
-	void SetMean(const string& mean_file);
+	void set_mean_from_file(const string& mean_file);
 
-	void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+	//wraps input layer's BLOBs in vector of opencv matrices for convinience
+	void wrap_input_layer(vector<cv::Mat>* input_channels);
 
-	void Preprocess(const cv::Mat& img,
-					std::vector<cv::Mat>* input_channels);
+	void preprocess_and_set_image(const cv::Mat& img,
+					vector<cv::Mat>* input_channels);
 
 private:
 	shared_ptr<Net<float> > net_;
@@ -69,11 +66,11 @@ Compressor::Compressor(const string& model_file,
 
 	if(mean_file != "") {
 		/* Load the binaryproto mean file. */
-		SetMean(mean_file);
+		set_mean_from_file(mean_file);
 	}
 }
 
-void Compressor::SetMean(const string& mean_file) {
+void Compressor::set_mean_from_file(const string& mean_file) {
 
 	BlobProto blob_proto;
 	ReadProtoFromBinaryFileOrDie(mean_file.c_str(), &blob_proto);
@@ -85,7 +82,7 @@ void Compressor::SetMean(const string& mean_file) {
 		<< "Number of channels of mean file doesn't match input layer.";
 
 	/* The format of the mean file is planar 32-bit float BGR or grayscale. */
-	std::vector<cv::Mat> channels;
+	vector<cv::Mat> channels;
 	float* data = mean_blob.mutable_cpu_data();
 	for (int i = 0; i < num_channels_; ++i) {
 		/* Extract an individual channel. */
@@ -104,44 +101,41 @@ void Compressor::SetMean(const string& mean_file) {
 	mean_ = cv::Mat(input_geometry_, mean.type(), channel_mean);
 }
 
-std::vector<float> Compressor::compress(const cv::Mat& img) {
-	Blob<float>* input_layer = net_->input_blobs()[0];
+vector<float> Compressor::compress(const cv::Mat& img) {
+
+	/* Copy the input layer to a vector and insert our image into it*/
+	Blob<float>* input_layer = net_->input_blobs()[0],
+				 *output_layer = net_->output_blobs()[0];
 	input_layer->Reshape(1, num_channels_,
 						 input_geometry_.height, input_geometry_.width);
 	/* Forward dimension change to all layers. */
 	net_->Reshape();
 
-	std::vector<cv::Mat> input_channels;
-	WrapInputLayer(&input_channels);
+	vector<cv::Mat> input_channels;
+	wrap_input_layer(&input_channels);
 
-	Preprocess(img, &input_channels);
+	preprocess_and_set_image(img, &input_channels);
 
+	float img_dim = input_layer->width();
+	//save a copy of image vector before compression
+	cv::Mat input_img(img_dim, img_dim, CV_32FC1, input_layer->mutable_cpu_data());
 	net_->Forward();
+	
+    std::cout << "---------- Storing decompressed image as rez.png   ----------" << std::endl;
+	cv::Mat decompressed_img(img_dim,img_dim, CV_32FC1, output_layer->mutable_cpu_data());
+	cv::imwrite("./rez.png", decompressed_img); 
 
-	/* Copy the output layer to a std::vector */
-	Blob<float>* output_layer = net_->output_blobs()[0];
+    float loss = norm(input_img, decompressed_img, cv::NORM_L2);
 
-	const float* begin = output_layer->cpu_data();
-	const float* end = begin + output_layer->count();
+    std::cout << "loss is " << loss << " (check this), img_dim is "<<img_dim << std::endl;
 
-	std::vector<float> decompression(begin, end);
+	shared_ptr<Blob<float>> compression_layer = net_->blob_by_name("encode1");
 
-	if(output_layer->width() > 1 || output_layer->height() > 1) {
-		std::cout << "Deploy.prototxt outputs a image of some sort, storing it as rez.png" << std::endl;
-		cv::imwrite("./rez.png", cv::Mat(output_layer->width(),output_layer->height(), TYPE, decompression.data())); 
-	} else if(output_layer->channels() > 3) {
+	const float* begin = compression_layer->cpu_data();
+	const float* end = begin + compression_layer->count();
 
-		std::cout<<(entering_image.size()==decompression.size())<<std::endl;
-
-		//TODO:count loss for image	
-
-		std::cout << "Deploy.prototxt outputs a decompressed image, storing it as rez.png" << std::endl;
-		cv::imwrite("./rez.png", cv::Mat((int)sqrt(output_layer->channels()),(int)sqrt(output_layer->channels()), 
-					CV_32FC1, decompression.data())); 
-	}
-
-	//TODO:extract middle and change this to compression
-	return decompression;
+	vector<float> compression(begin, end);
+	return compression;
 }
 
 /* Wrap the input layer of the network in separate cv::Mat objects
@@ -149,7 +143,7 @@ std::vector<float> Compressor::compress(const cv::Mat& img) {
  * don't need to rely on cudaMemcpy2D. The last preprocessing
  * operation will write the separate channels directly to the input
  * layer. */
-void Compressor::WrapInputLayer(std::vector<cv::Mat>* input_channels) {
+void Compressor::wrap_input_layer(vector<cv::Mat>* input_channels) {
 	Blob<float>* input_layer = net_->input_blobs()[0];
 
 	int width = input_layer->width();
@@ -162,8 +156,8 @@ void Compressor::WrapInputLayer(std::vector<cv::Mat>* input_channels) {
 	}
 }
 
-void Compressor::Preprocess(const cv::Mat& img,
-                            std::vector<cv::Mat>* input_channels) {
+void Compressor::preprocess_and_set_image(const cv::Mat& img,
+                            vector<cv::Mat>* input_channels) {
 	/* Convert the input image to the input image format of the network. */
 	cv::Mat sample;
 	if (img.channels() == 3 && num_channels_ == 1)
@@ -198,114 +192,35 @@ void Compressor::Preprocess(const cv::Mat& img,
 	 * objects in input_channels. */
 	cv::split(sample_normalized, *input_channels);
 
-	Blob<float>* input_layer = net_->input_blobs()[0];
-
-    const float* begin = input_layer->cpu_data();
-    const float* end = begin + input_layer->count();
-
-    vector<float> entering(begin, end);
-
-    std::cout << "entering size is " <<entering.size() << std::endl;
-	entering_image = entering;
-
-    if(input_layer->width() > 1 || input_layer->height() > 1) {
-        std::cout << "Saving image as entering.png" << std::endl;
-        cv::Mat mat_entering(input_layer->width(),input_layer->height(), CV_32FC1, entering.data());
-        cv::imwrite("./entering.png", mat_entering);
-    }
-
 	CHECK(reinterpret_cast<float*>(input_channels->at(0).data)
 		  == net_->input_blobs()[0]->cpu_data())
 		  << "Input channels are not wrapping the input layer of the network.";
 }
 
-//TODO: switch from this to compressing and decompressing in compress()
-void Compressor::decompress() {
-
-	std::vector<float> compression;
-	std::ifstream in("compressed.txt");
-	float val;
-
-	in >> val;
-	while(!in.eof()) {
-		compression.push_back(val);
-		in >> val;
-	}
-
-	in.close();
-
-	if(compression.data())
-		net_->input_blobs()[0]->set_cpu_data( compression.data() );
-
-	net_->Forward();
-
-    /* Copy the output layer to a std::vector */
-    Blob<float>* output_layer = net_->output_blobs()[0];
-	
-	const float* begin = output_layer->cpu_data();
-	const float* end = begin + output_layer->count();
-
-	std::vector<float> decompression(begin, end);
-	float* diff = new float[decompression.size()];
-	std::cout << decompression.size() << ", " << entering_image.size() << std::endl;
-	if(!(decompression.size()==entering_image.size() && decompression.data() && entering_image.data()))
-		std::cout<<"ne valja nesto!!!!"<<std::endl;
-	caffe_sub(decompression.size(), decompression.data(), entering_image.data(), diff);
-	float loss = caffe_cpu_dot(decompression.size(), diff, diff) / 2;
-	std::cout<<"loss is "<<loss<<std::endl;
-
-	delete[] diff;
-
-	if(output_layer->width() > 1 || output_layer->height() > 1) {
-		std::cout << "Saving image as decompression.png" << std::endl;
-		cv::Mat mat_decompression(output_layer->width(),output_layer->height(), TYPE, decompression.data());
-		cv::imwrite("./decompression.png", mat_decompression); 
-  	} else if(output_layer->channels()>3) {
-		std::cout << "Saving image as decompression.png" << std::endl;
-		cv::Mat mat_decompression((int)sqrt(output_layer->channels()),(int)sqrt(output_layer->channels()), TYPE, decompression.data());
-		cv::imwrite("./decompression.png", mat_decompression); 
-	}
-}
-
 int main(int argc, char** argv) { 
-	//TODO: lose -d option, keep only option for deploy of whole network
 	if (argc != 5 && argc != 4) { 
 		std::cerr << "Usage: " << argv[0] 
-				  << " [-d] deploy.prototxt network.caffemodel" 
-				  << " [mean.binaryproto img.jpg]" << std::endl; 
+				  << " deploy.prototxt network.caffemodel" 
+				  << " mean.binaryproto img.jpg" << std::endl; 
 		return 1; 
 	} 
 
-	::google::InitGoogleLogging(argv[0]); 
-
-	//TODO:lose this
-	if(argc == 5 && argv[1] == std::string("-d")) {
-
-		string model_file   = argv[2]; 
-		string trained_file = argv[3]; 
-		Compressor compressor(model_file, trained_file, ""); 
-
-		string file = argv[4]; 
-		cv::Mat img;
-
-		compressor.decompress();
-		return 0;
-	} 
+	::google::InitGoogleLogging("compression"); 
 
 	string model_file   = argv[1]; 
 	string trained_file = argv[2]; 
 	string mean_file    = argv[3]; 
+	string image = argv[4]; 
 	Compressor compressor(model_file, trained_file, mean_file); 
 
-	string file = argv[4]; 
 	cv::Mat img;
-	img = cv::imread(file, -1);
+	img = cv::imread(image, -1);
 
-	auto compressed = compressor.compress(img);
+	vector<float> compressed = compressor.compress(img);
 
 	std::ofstream out("compressed.txt");
 
-	for(auto a:compressed)
+	for(float a : compressed)
 		out << a << " ";
 
 	std::cout << "---------- Saved compressed file to compressed.txt ----------" << std::endl; 

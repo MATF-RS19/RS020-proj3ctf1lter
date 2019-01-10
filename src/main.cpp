6ebcdcefc94a12026356d5dd0f9bfe0c485699a9
deMand_A <mandinikola@gmail.com>
2019-01-10 13:26:06 +0100

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,8 @@
 #include <caffe/caffe.hpp>
-#ifdef USE_OPENCV
-#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#endif  // USE_OPENCV
-#include <algorithm>
-#include <numeric>
-#include <iosfwd>
-#include <memory>
-#include <string>
-#include <utility>
-#include <vector>
-#include <ostream>
 
-#ifdef USE_OPENCV
-using namespace caffe;  // NOLINT(build/namespaces)
+using namespace caffe;
 using std::string;
 
 vector<float> entering_image;
@@ -197,20 +185,9 @@ void Compressor::preprocess_and_set_image(const cv::Mat& img,
 		  << "Input channels are not wrapping the input layer of the network.";
 }
 
-int main(int argc, char** argv) { 
-	if (argc != 5 && argc != 4) { 
-		std::cerr << "Usage: " << argv[0] 
-				  << " deploy.prototxt network.caffemodel" 
-				  << " mean.binaryproto img.jpg" << std::endl; 
-		return 1; 
-	} 
-
+void compression(string model_file, string trained_file, string mean_file, string image) {
 	::google::InitGoogleLogging("compression"); 
 
-	string model_file   = argv[1]; 
-	string trained_file = argv[2]; 
-	string mean_file    = argv[3]; 
-	string image = argv[4]; 
 	Compressor compressor(model_file, trained_file, mean_file); 
 
 	cv::Mat img;
@@ -226,12 +203,22 @@ int main(int argc, char** argv) {
 	std::cout << "---------- Saved compressed file to compressed.txt ----------" << std::endl; 
 
 	out.close();
+}
+
+int main(int argc, char** argv) { 
+	if (argc != 5 && argc != 4) { 
+		std::cerr << "Usage: " << argv[0] 
+				  << " deploy.prototxt network.caffemodel" 
+				  << " mean.binaryproto img.jpg" << std::endl; 
+		return 1; 
+	} 
+
+	string model_file   = argv[1]; 
+	string trained_file = argv[2]; 
+	string mean_file    = argv[3]; 
+	string image        = argv[4]; 
+
+	compression( model_file,  trained_file,  mean_file,  image);
 
 	return 0;
 }
-
-#else
-int main(int argc, char** argv) {
-	LOG(FATAL) << "This test requires OpenCV; compile with USE_OPENCV.";
-}
-#endif  // USE_OPENCV
